8fc3f4e00712a25783013cb351e98a5c991e1f48
fangzhao <fangzhao1123@gmail.com>
2024-09-25 18:52:49 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/cc17_3k_fangzhao_act3/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.cc17_3k_fangzhao_act3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val roll : Button = findViewById<Button>(R.id.button)
+
+        val rollImage : ImageView = findViewById<ImageView>(R.id.imageView)
+
+        roll.setOnClickListener {
+            val randomNumber = (1..6).random()
+
+
+            val changable = when (randomNumber) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            rollImage.setImageResource(changable)}
+    }
+}
